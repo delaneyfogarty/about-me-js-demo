ce62eb473114ad67a2a0162498b8cc5bd2748957
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-01 15:45:33 -0800

    snail button finally works

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 // no functions to import
 
 // grab DOM elements
-const showButton = document.getElementById(animal);
-const animal = document.getElementById(animalDiv);
+const showButton = document.getElementById('btn');
+const animal = document.getElementById('animalDiv');
 
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
-showButton.addEventListener('click', (animal)) => {
-    animal.classList.remove('hidden');}
+showButton.addEventListener('click', () => {
+    animal.classList.remove('hidden');
+});
 
